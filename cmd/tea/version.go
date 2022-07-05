a9dd78e1b89d351b7d3bf315687149a8b089a80b
Marek Cermak <marek.cermak@strv.com>
2022-07-05 15:49:13 +0200

    feat: add version command
    Signed-off-by: Marek Cermak <marek.cermak@strv.com>

---
+++ cmd/tea/version.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var version string // version is set during build
+
+var (
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Show version and exit",
+		Long: `This command displays current version and exits.
+
+Example:
+	tea version
+
+Provided by ` + colorLinkSTRV,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(version)
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
