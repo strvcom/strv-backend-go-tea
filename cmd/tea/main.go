7d85ca8ece55e512daab118601106355f36b3857
Matúš Bafrnec <matus.bafrnec@gmail.com>
2026-03-30 10:41:33 +0200

    chore: update dependencies, Go 1.26
    Signed-off-by: Matúš Bafrnec <matus.bafrnec@gmail.com>

--- cmd/tea/main.go
+++ cmd/tea/main.go
@@ -11,8 +11,7 @@ func main() {
 	// Execute adds all child commands to the root command and sets flags appropriately.
 	// This is called by main.main(). It only needs to happen once to the rootCmd.
 	if err := rootCmd.Execute(); err != nil {
-		e := &cmderrors.CommandError{}
-		if errors.As(err, &e) {
+		if e, ok := errors.AsType[*cmderrors.CommandError](err); ok {
 			os.Exit(e.Code)
 		}
 		os.Exit(1)
